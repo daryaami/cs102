0baff497ba56d21a68738ed07790165d00237762
daryaami <daryaamii@mail.ru>
2023-09-18 20:49:03 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -26,8 +26,12 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while a > 0 and b > 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -49,11 +53,11 @@ def generate_keypair(
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = p*q
-    # PUT YOUR CODE HERE
+    # n = pq
+    n = p * q
 
     # phi = (p - 1)(q - 1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
