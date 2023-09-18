4f666ae7a0da17d26316110224479d278cccd333
daryaami <daryaamii@mail.ru>
2023-09-18 13:18:15 +0300

    Добавлено решение

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,8 +1,10 @@
-'''Calculator'''
+"""Calculator"""
+
 
 def divide(num, denom):
-    '''Function dividing two numbers'''
+    """Function dividing two numbers"""
     return num / denom
+
 
 numerator = float(input())
 denominator = float(input())
