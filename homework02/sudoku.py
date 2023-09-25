96f07e37ed7a42180f1d8e330b0a3bec282ddba5
daryaami <daryaamii@mail.ru>
2023-09-25 12:27:26 +0300

    add function group

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,7 +41,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [[values[i*n:(i+1)*n]] for i in range(n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
