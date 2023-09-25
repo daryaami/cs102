dc666abc67cc5a6a8725e93b70b2bc39792e7eab
daryaami <daryaamii@mail.ru>
2023-09-25 17:30:36 +0300

    add function solve

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,7 +41,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    return [values[i*n:(i+1)*n] for i in range(n)]
+    return [values[i * n : (i + 1) * n] for i in range(n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -78,9 +78,9 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    row = pos[0]
-    col = pos[1]
-    return [grid[int(row/3)*3+i][int(col/3)*3+j] for i in range(3) for j in range(3)]
+    row = pos[0] // 3 * 3
+    col = pos[1] // 3 * 3
+    return [grid[i][j] for i in range(row, row + 3) for j in range(col, col + 3)]
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
@@ -92,7 +92,11 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == '.':
+                return (i, j)
+    return None
 
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
@@ -105,7 +109,11 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    numbers = '123456789'
+    row = [s for s in get_row(grid, pos)]
+    col = [s for s in get_col(grid, pos)]
+    block = [s for s in get_block(grid, pos)]
+    return set(numbers) - set(row + col + block)
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
@@ -120,7 +128,22 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    empty_position = find_empty_positions(grid)
+
+    if not empty_position:
+        return grid
+
+    row, col = empty_position
+    posible_values = find_possible_values(grid, empty_position)
+
+    if posible_values:
+        for opt in posible_values:
+            grid[row][col] = opt
+            if solve(grid):
+                return grid
+            grid[row][col] = '.'
+    
+    return None
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
